24a9096c5d5c794d020723b0637ed24228edd45c
DeveloperSujal <sujal7455@gmail.com>
2024-08-01 11:28:55 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+    <StrictMode>
+        <App />
+    </StrictMode>
+
